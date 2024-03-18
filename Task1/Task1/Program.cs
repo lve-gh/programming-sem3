c47bc9650145509e4e691a047536dac883023d49
lve-gh <lve.github@outlook.com>
2024-03-18 13:16:51 +0300

    fixed remarks

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -21,17 +21,10 @@ matrix3 = MatrixMultiplicationClass.MultiplicationConcurent(matrix1, matrix2);
 
 if (matrix3 != null)
 {
-    for (int i = 0; i < matrix3.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix3.Length / matrix3.GetLength(0); j++)
-        {
-            Console.WriteLine(matrix3[i, j]);
-        }
-    }
     Console.WriteLine("Введите путь сохранения матрицы:");
     path = args[2];
     path ??= "";
     MatrixWorkFileClass.MatrixWrite(matrix3, path);
 }
  else
-    Console.WriteLine("Ошибка выполнения умножения");
+    throw new Exception("Ошибка выполнения умножения");
